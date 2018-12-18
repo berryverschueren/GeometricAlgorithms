05a14aed8fa664e01e87ab7712ba06d6f8882a4d
berryverschueren <berryverschueren@gmail.com>
2018-12-18 15:32:42 +0100

    Update Polygon.java

--- GEO/src/geo/dataStructures/Polygon.java
+++ GEO/src/geo/dataStructures/Polygon.java
@@ -5,10 +5,48 @@
  */
 package geo.dataStructures;
 
+import java.util.List;
+
 /**
  *
  * @author Berry-PC
  */
 public class Polygon {
+    private String label;
+    private List<Edge> edges;
+    private List<Vertex> vertices;
+
+    public Polygon() {
+    }
+
+    public Polygon(String label, List<Edge> edges, List<Vertex> vertices) {
+        this.label = label;
+        this.edges = edges;
+        this.vertices = vertices;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public List<Edge> getEdges() {
+        return edges;
+    }
+
+    public List<Vertex> getVertices() {
+        return vertices;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public void setEdges(List<Edge> edges) {
+        this.edges = edges;
+    }
+
+    public void setVertices(List<Vertex> vertices) {
+        this.vertices = vertices;
+    }
     
 }
