d86beef2cc4347f1e73cf858762e14d3713d023f
berryverschueren <berryverschueren@gmail.com>
2018-12-18 15:30:38 +0100

    Update Edge.java

--- GEO/src/geo/dataStructures/Edge.java
+++ GEO/src/geo/dataStructures/Edge.java
@@ -10,5 +10,40 @@ package geo.dataStructures;
  * @author Berry-PC
  */
 public class Edge {
+    private String label;
+    private Vertex v1, v2;
+
+    public Edge() {
+    }
+
+    public Edge(String label, Vertex v1, Vertex v2) {
+        this.label = label;
+        this.v1 = v1;
+        this.v2 = v2;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public Vertex getV1() {
+        return v1;
+    }
+
+    public Vertex getV2() {
+        return v2;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public void setV1(Vertex v1) {
+        this.v1 = v1;
+    }
+
+    public void setV2(Vertex v2) {
+        this.v2 = v2;
+    }
     
 }
