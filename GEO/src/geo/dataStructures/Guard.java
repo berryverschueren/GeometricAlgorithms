fa5a69683e3cfa613584d42cd51a8f138abc65f5
berryverschueren <berryverschueren@gmail.com>
2018-12-18 15:34:58 +0100

    Added guard and robber class

---
+++ GEO/src/geo/dataStructures/Guard.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geo.dataStructures;
+
+/**
+ *
+ * @author Berry-PC
+ */
+public class Guard {
+    private String label;
+    private int x, y;
+
+    public Guard() {
+    }
+
+    public Guard(String label, int x, int y) {
+        this.label = label;
+        this.x = x;
+        this.y = y;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+}
