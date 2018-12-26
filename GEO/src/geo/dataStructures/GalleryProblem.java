2144e936a7877aca9e99d6b957a289d177cf9d3f
Skaj7 <kaj753@hotmail.com>
2018-12-26 18:31:39 +0100

    gallery

---
+++ GEO/src/geo/dataStructures/GalleryProblem.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geo.dataStructures;
+
+/**
+ *
+ * @author Kaj75
+ */
+public class GalleryProblem {
+    private Gallery gellery;
+    private int guards;
+    private int speed;
+    private int globalTime;
+    private int observationTime;
+    
+    
+}
