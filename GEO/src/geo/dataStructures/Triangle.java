2ab7cad1a7479d0aea16327e3834d82ce0918d8c
berryverschueren <berryverschueren@gmail.com>
2019-01-08 16:23:46 +0100

    Update TrapezoidalMap Triangulation

---
+++ GEO/src/geo/dataStructures/Triangle.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geo.dataStructures;
+
+/**
+ *
+ * @author Berry-PC
+ */
+public class Triangle extends TrapezoidShape {
+    // Coordinates
+    private Vertex v1, v2, v3;
+    private Edge e1, e2, e3;
+    private int c1, c2, c3;
+    
+    public Triangle() {
+        this.c1 = 0;
+        this.c2 = 0;
+        this.c3 = 0;
+    }
+
+    public Triangle(String label, Vertex v1, Vertex v2, Vertex v3, Edge e1, Edge e2, Edge e3) {
+        super(label);
+        this.v1 = v1;
+        this.v2 = v2;
+        this.v3 = v3;
+        this.e1 = e1;
+        this.e2 = e2;
+        this.e3 = e3;
+        this.c1 = 0;
+        this.c2 = 0;
+        this.c3 = 0;
+    }
+
+    public void setC1(int c1) {
+        this.c1 = c1;
+    }
+
+    public void setC2(int c2) {
+        this.c2 = c2;
+    }
+
+    public void setC3(int c3) {
+        this.c3 = c3;
+    }
+
+    public int getC1() {
+        return c1;
+    }
+
+    public int getC2() {
+        return c2;
+    }
+
+    public int getC3() {
+        return c3;
+    }
+
+    public Vertex getV1() {
+        return v1;
+    }
+
+    public Vertex getV2() {
+        return v2;
+    }
+
+    public Vertex getV3() {
+        return v3;
+    }
+    
+    public Edge getE1() {
+        return e1;
+    }
+
+    public Edge getE2() {
+        return e2;
+    }
+
+    public Edge getE3() {
+        return e3;
+    }
+
+    public void setV1(Vertex v1) {
+        this.v1 = v1;
+    }
+
+    public void setV2(Vertex v2) {
+        this.v2 = v2;
+    }
+
+    public void setV3(Vertex v3) {
+        this.v3 = v3;
+    }
+
+    public void setE1(Edge e1) {
+        this.e1 = e1;
+    }
+
+    public void setE2(Edge e2) {
+        this.e2 = e2;
+    }
+
+    public void setE3(Edge e3) {
+        this.e3 = e3;
+    }
+    
+    @Override
+    public void print() {
+        System.out.println("Triangle " + this.getLabel() + ": ((" + this.v1.getX() + ", " + this.v1.getY() + "), (" + this.v2.getX() + ", " + this.v2.getY() + "), (" + 
+                this.v3.getX() + ", " + this.v3.getY() + "))");
+    }
+    
+}
